4057104b9638c735dbbbe5445c326deae256bd37
Denny Septian Panggabean <97607754+ddevsr@users.noreply.github.com>
2024-04-01 21:48:38 +0700

    Create rector.php

---
+++ rector.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of PHPDevsr/recaptcha-codeigniter4.
+ *
+ * (c) 2023 Denny Septian Panggabean <xamidimura@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use PHPDevsr\Rector\Codeigniter4\Set\CodeigniterSetList;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector;
+use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
+use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
+use Rector\Php73\Rector\FuncCall\StringifyStrNeedlesRector;
+use Rector\Php80\Rector\Class_\AnnotationToAttributeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->withSets([
+        CodeigniterSetList::CODEIGNITER_44
+    ]);
+
+    // The paths to refactor (can also be supplied with CLI arguments)
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/tests',
+    ]);
+
+    // Include Composer's autoload - required for global execution, remove if running locally
+    $rectorConfig->autoloadPaths([
+        __DIR__ . '/vendor/autoload.php',
+    ]);
+
+    // Do you need to include constants, class aliases, or a custom autoloader?
+    $rectorConfig->bootstrapFiles([
+        realpath(getcwd()) . '/vendor/codeigniter4/framework/system/Test/bootstrap.php',
+    ]);
+
+    if (is_file(__DIR__ . '/phpstan.neon.dist')) {
+        $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon.dist');
+    }
+
+    // Are there files or rules you need to skip?
+    $rectorConfig->skip([
+        __DIR__ . '/app/Views',
+
+        JsonThrowOnErrorRector::class,
+        StringifyStrNeedlesRector::class,
+
+        // Note: requires php 8
+        RemoveUnusedPromotedPropertyRector::class,
+
+        // May load view files directly when detecting classes
+        StringClassNameToClassConstantRector::class,
+
+        AnnotationToAttributeRector::class,
+    ]);
+};
